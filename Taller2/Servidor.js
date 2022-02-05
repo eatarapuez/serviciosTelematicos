35c602dbe7a756abcb5c046b046278983ac36dfa
Alejandro <alejao@test.com>
2022-02-05 17:53:43 -0500

    Taller 5

--- Taller2/Servidor.js
+++ Taller2/Servidor.js
@@ -1,45 +1,17 @@
-var express = require('express');   //Express Web Server 
-var bodyParser = require('body-parser'); //connects bodyParsing middleware
-var formidable = require('formidable');
-var path = require('path');     //used for file path
-var fs =require('fs-extra');    //File System-needed for renaming file etc
-
-var app = express();
-app.use(express.static(path.join(__dirname, 'public')));
-
-/* ========================================================== 
- bodyParser() required to allow Express to see the uploaded files
-============================================================ */
-app.use(bodyParser({defer: true}));
- app.route('/upload')
- .post(function (req, res, next) {
-
-  var form = new formidable.IncomingForm();
-    //Formidable uploads to operating systems tmp dir by default
-    form.uploadDir = "./img";       //set upload directory
-    form.keepExtensions = true;     //keep file extension
-
-    form.parse(req, function(err, fields, files) {
-        res.writeHead(200, {'content-type': 'text/plain'});
-        res.write('received upload:\n\n');
-        console.log("form.bytesReceived");
-        //TESTING
-        console.log("file size: "+JSON.stringify(files.fileUploaded.size));
-        console.log("file path: "+JSON.stringify(files.fileUploaded.path));
-        console.log("file name: "+JSON.stringify(files.fileUploaded.name));
-        console.log("file type: "+JSON.stringify(files.fileUploaded.type));
-        console.log("astModifiedDate: "+JSON.stringify(files.fileUploaded.lastModifiedDate));
-
-        //Formidable changes the name of the uploaded file
-        //Rename the file to its original name
-        fs.rename(files.fileUploaded.path, './img/'+files.fileUploaded.name, function(err) {
-        if (err)
-            throw err;
-          console.log('renamed complete');  
-        });
-          res.end();
-    });
+//Importe el módulos
+const express = require("express");
+const app = express();
+const puerto = 5000;
+//Motor de plantillas
+app.set('view engine', 'ejs');
+app.set('views',__dirname+'/views');
+//Atención de la solicitud
+app.get('/', (req,res)=>{
+    res.render("index", {titulo:"Don titulo",
+                        parrafo:"Este es un parráfo de ejemplo.",
+                        Variable_n:"Esta es la n-ésima variable."})
 });
-var server = app.listen(3030, function() {
-console.log('Listening on port %d', server.address().port);
+//Oyente
+app.listen(puerto, () => {
+    console.log("Ejecutando express");
 });
